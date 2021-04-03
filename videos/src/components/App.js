f2ff2083e10969ca0a717d06193952c45781dae5
carycheng <carycheng77@gmail.com>
2021-04-03 12:01:34 -0700

    working youtube api

---
+++ videos/src/components/App.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import SearchBar from './SearchBar';
+import youtube from '../apis/youtube';
+
+class App extends React.Component {
+
+    onTermSubmit = async (term) => {
+        console.log(term);
+
+        const results = await youtube.get('/search', {
+            params: {
+                q: term
+            }
+        });
+
+        console.log(results);
+    };
+
+    render() {
+        return (
+            <div className="ui container">
+                <SearchBar onFormSubmit={this.onTermSubmit}/>
+            </div>
+        );
+    }
+}
+
+export default App;
