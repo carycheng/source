b97b23ed36892d725c4b99a3ee630a8f5c89e739
carycheng <carycheng77@gmail.com>
2021-05-01 11:01:21 -0700

    added description

--- videos-hooks/src/hooks/useVideos.js
+++ videos-hooks/src/hooks/useVideos.js
@@ -1,6 +1,7 @@
 import {useState, useEffect} from 'react';
 import youtube from '../apis/youtube';
 
+// useVideos custom hook
 const useVideos = (defaultSearchTerm) => {
     const [videos, setVideos] = useState([]);
 
