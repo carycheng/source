c4ff1a5f58c2942f1d42e545318b5d6e0e1d93c9
carycheng <carycheng77@gmail.com>
2021-05-12 20:48:18 -0700

    adding notes for combinereducers

--- blog/src/reducers/postsReducer.js
+++ blog/src/reducers/postsReducer.js
@@ -2,6 +2,12 @@
 // and loops through every reducer. It calls dispatch with the action on every reducer
 // and returns a state. If at least one reducer matched the action and updated the state
 // then the hasChanged flag is flipped which eventually kicks off a rerender of the page.
-export default () => {
-    return 123;
+// to see this in action go to Lecture 267.
+export default (state = [], action) => {
+    switch (action.type) {
+        case 'FETCH_POSTS':
+            return action.payload;
+        default:
+            return state;
+    }
 };
