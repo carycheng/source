3f713ca010ef254c54bf5cfa860557ff2a4b0020
carycheng <carycheng77@gmail.com>
2021-07-30 21:52:56 -0700

    switched redux to context

--- translate/src/component/App.js
+++ translate/src/component/App.js
@@ -1,7 +1,8 @@
 import React from 'react';
 import UserCreate from './UserCreate';
-import LanguageContext from '../contexts/LanguageContext';
+import { LanguageStore } from '../contexts/LanguageContext';
 import ColorContext from '../contexts/ColorContext';
+import LanguageSelector from './LanguageSelector';
 
 class App extends React.Component {
 
@@ -14,17 +15,12 @@ class App extends React.Component {
     render() {
         return(
             <div className="ui container">
-                <div>
-                    Select a language:
-                    {/* Arrow functions have context of the class so don't need to explicity bind */}
-                    <i className="flag us" onClick={() => this.onLanguageChange('english')} />
-                    <i className="flag nl" onClick={() => this.onLanguageChange('dutch')} />
-                </div>
-                <ColorContext.Provider value="red">
-                    <LanguageContext.Provider value={ this.state.language }>
+                <LanguageStore>
+                    <LanguageSelector/>
+                    <ColorContext.Provider value="red">
                         <UserCreate />
-                    </LanguageContext.Provider>
-                </ColorContext.Provider>
+                    </ColorContext.Provider>
+                </LanguageStore>
             </div>
         );
     }
