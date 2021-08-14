6bc5618dc7a4e816b681db94f216982f6d7fc514
carycheng <carycheng77@gmail.com>
2021-08-14 12:04:36 -0700

    added preview component

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,12 +1,18 @@
 import logo from './logo.svg';
 import './App.css';
 
+import { Switch, Route } from 'react-router-dom';
+
 import HomePage from './pages/homepage/homepage.component.jsx';
+import ShopPage from './pages/shop/shop.component.jsx';
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Switch>
+        <Route exact path='/' component={ HomePage } />
+        <Route path='/path' component={ ShopPage } />
+      </Switch>
     </div>
   );
 }
